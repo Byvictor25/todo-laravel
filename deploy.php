41a16b1a573e0d4791bb146644617dfc60e748d5
Victor Perez Domenech <victorrperez25@gmail.com>
2023-01-15 18:42:05 +0100

    [*ADD] Feature a43534sg – CRUD usuaris

--- deploy.php
+++ deploy.php
@@ -4,10 +4,10 @@ namespace Deployer;
 require 'recipe/laravel.php';
 
 // Project name
-set('application', 'my_project');
+set('application', 'desplegar_laravel');
 
 // Project repository
-set('repository', 'git@domain.com:username/repository.git');
+set('repository', 'git@github.com:Byvictor25/todo-laravel.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
@@ -22,9 +22,10 @@ add('writable_dirs', []);
 
 // Hosts
 
-host('project.com')
-    ->set('deploy_path', '~/{{application}}');    
-    
+host('192.168.56.107')
+    ->set('deploy_path', '/var/www/prog-ud4-a4/html')
+    ->identityFile('~/.ssh/id_rsa.pub')
+    ->user('ddaw-ud4-deployer');
 // Tasks
 
 task('build', function () {
@@ -38,3 +39,10 @@ after('deploy:failed', 'deploy:unlock');
 
 before('deploy:symlink', 'artisan:migrate');
 
+// Declaració de la tasca
+task('reload:php-fpm', function () {
+ run('sudo /etc/init.d/php8.1-fpm restart');
+});
+
+// inclusió en el cicle de desplegament
+after('deploy', 'reload:php-fpm');
